b89d96afa9af055758a1f6c280236394e8b26855
Vladimir Babich <vladimirbabich18@gmail.com>
2023-05-20 12:14:46 +0300

    add require pg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const pg = require('pg');
 const models = require('./models/databaseModels');
 const cors = require('cors');
 const router = require('./routes/index');
