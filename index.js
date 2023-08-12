2a9f925d8aa569f09aca066eca3b592fa3a76a1f
Vladimir Babich <vladimirbabich18@gmail.com>
2023-08-12 16:00:44 +0300

    returned origin statement, for now

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const whitelist = ['https://retester.tech', 'https://regexp-tester.vercel.app'];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (origin === undefined || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
